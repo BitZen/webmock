693775d86602c2605ac069c8de5f8aa5eeec4247
rrushing <reece.rushing@ryder.com>
2025-09-15 12:42:44 -0400

    adds env var for optional error verbosity

--- lib/webmock/errors.rb
+++ lib/webmock/errors.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 module WebMock
-
   class NetConnectNotAllowedError < Exception
     def initialize(request_signature)
       request_signature_snippet = RequestSignatureSnippet.new(request_signature)
-      text = [
-        "Real HTTP connections are disabled. Unregistered request: #{request_signature}",
-        request_signature_snippet.stubbing_instructions,
-        request_signature_snippet.request_stubs,
-        "="*60
-      ].compact.join("\n\n")
-      super(text)
+
+      lines = []
+      lines << "Real HTTP connections are disabled. Unregistered request: #{request_signature}"
+
+      if ENV['WEBMOCK_VERBOSE'].to_bool
+        lines << request_signature_snippet.stubbing_instructions
+        lines << request_signature_snippet.request_stubs
+      end
+
+      lines << '=' * 60
+
+      super(lines.compact.join("\n\n"))
     end
-
   end
-
 end
